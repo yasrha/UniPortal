2fda84f95285db072a5f1c701b23ba12dce5e7c3
yasrha <yasinr923@gmail.com>
2023-09-15 11:47:54 -0400

    Added a StudentDTO

---
+++ UniPortal/DataTransferObjects/StudentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPortal.DataTransferObjects
+{
+    public class StudentDTO
+    {
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public string PasswordHash {  get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public DateTime DateOfBirth {  get; set; }
+    }
+}
